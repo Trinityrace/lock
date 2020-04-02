e8113f5b44ec311d8ca1d687605aedcffded7c61
Trinity John <trinityrtime@gmail.com>
2020-04-02 15:48:26 +0300

    unittests for credentials
    total of five tests for credentials update

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
-import random
 import math
+import random
 import pyperclip
 import string
 
@@ -7,13 +7,14 @@ class Credentials(object):
     """
     Class that generates new instances of credentials
     """
-        
+
+    credentials_list = [] #empty credentials list        
     def __init__(self,platform,username,password):
         '''
         __init__ method that helps us define properties for our objects.
 
         Args:
-            paltform: New user platform.
+            platform: New user platform.
             username : New user username.
             password: New user password.
             
@@ -22,7 +23,16 @@ class Credentials(object):
         self.username = username
         self.password = password
 
+    #save_user method to append on credentials_list
+    def save_credentials(self):
+        '''
+        save_credentials method saves objects into credentials_list
+        '''
+        Credentials.credentials_list.append(self)
 
-    #pass
+    
+
+
+#    pass
 
 
--- credentials_test.py
+++ credentials_test.py
@@ -1,4 +1,7 @@
 import unittest
+import random
+import pyperclip
+
 from credentials import Credentials
 
 class TestCredentials(unittest.TestCase):
@@ -12,6 +15,44 @@ class TestCredentials(unittest.TestCase):
         self.assertEqual(self.new_credential.platform,"instagram")
         self.assertEqual(self.new_credential.username,"trinity")
         self.assertEqual(self.new_credential.password,"123")
+
+    def test_save_credentials(self):
+        self.new_credential.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),1)
+
+    def tearDown(self):
+        '''
+        does clean up after every test has run
+        '''
+        Credentials.credentials_list=[]
+
+    def test_save_multiple_credentials(self):
+        '''
+        test to check whether we can save multiple credentials
+        '''
+        self.new_credential.save_credentials()
+        test_credentials=Credentials("pinterest","khalifa","420")
+        test_credentials.save_credentials()
+        self.assertEqual(len(Credentials.credentials_list),2)
+
+    def test_delete_credentials(self):
+        '''
+        test to check deletion of credentials
+        '''
+        self.new_credential.save_credentials()
+        test_credentials=Credentials("twitter","marqeez","0420")
+        test_credentials.save_credentials()
+
+        self.new_credential.delete_credentials #delete credential object
+        self.assertEqual(len(Credentials.credentials_list),1)
+    
+    def test_find_username(self):
+        '''
+        test to find username and display credentials
+        '''
+        self.new_credential.save_credentials()
+        test_credentials=Credentials("twitter","marqeez","0420")
+
 
 if __name__ == '__main__':
     unittest.main()
