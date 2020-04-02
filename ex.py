fd4529735325683d52717341dcfe1d6949fa2b3b
Trinity John <trinityrtime@gmail.com>
2020-04-02 13:03:00 +0300

    initial
    created initial files for project

---
+++ ex.py
@@ -0,0 +1,71 @@
+import random
+import math
+import sys
+# import this
+
+numbers = [1,2,3,4,5]
+for number in numbers:
+    print(number)
+
+
+# name = ""
+# list_a = [1]
+
+# if list_a:
+#     print("I will not run")
+# else:
+#     print("I am Empty")
+
+##circumference calc
+# n=3.14
+# r = random.randint(0,10)
+# C=2*n*r
+# print(C)
+
+
+
+# ##BMI calc
+# weight=random.randint(0,200)
+# print('kilo ngapi?')
+# weight=int(input())
+# print('whats ya name?')
+# # name=sys.argv[1]
+# name=input()
+# print('whats ya height?')
+# height=int(input())
+
+# mass=(weight/height)
+# if mass >1:
+#     print (name+' yo overweight'+' ,uko '+str(mass))
+# elif mass==1:
+#     print (name +' yo good '+ ' uko '+mass)
+# else:
+#     print(name+' kula unone'+' ,uko '+str(mass))
+
+
+# name=sys.argv[1]
+# print('insert number')
+# weight=int(input())
+# print (name)
+# print(weight+'kgs')
+
+##birth year calculator
+# name=sys.argv[1]
+# print('age?')
+# age=int(input())
+# year=str((2020-age))
+# print(name+' you were born the year '+year)
+
+##age calculator
+# print('birth year?')
+# year=int(input())
+# age=str((2020-year))
+# print(name+' you are '+ age+ ' years old')
+
+# ##tip calculator
+# name=sys.argv[1]
+# print('total bill?')
+# bill=int(input())
+# tip=(0.15 * bill + bill)
+# total_cost=str(tip)
+# print(name+' total cost is '+total_cost+' plus tip, thank you')
