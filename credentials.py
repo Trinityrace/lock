136ce4fa70e662e345ed4d39e5cb0b4210292e49
Trinity John <trinityrtime@gmail.com>
2020-04-02 13:19:53 +0300

    feature details
    in class for credentials in credentials.py added class instances and object properties

--- credentials.py
+++ credentials.py
@@ -1,9 +1,28 @@
 import random
 import math
+import pyperclip
+import string
 
-class Credentials:
+class Credentials(object):
     """
     Class that generates new instances of credentials
     """
-    pass
+        
+    def __init__(self,platform,username,password):
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            paltform: New user platform.
+            username : New user username.
+            password: New contact password.
+            
+        '''
+        self.platform = platform
+        self.username = username
+        self.password = password
+
+
+    #pass
+
 
