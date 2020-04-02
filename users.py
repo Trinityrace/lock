86b8043fa66222c02c2e408ce78624f1cf00f4fa
Trinity John <trinityrtime@gmail.com>
2020-04-02 13:10:44 +0300

    feature class
    created class for users  in users.py with class instances and object properties

---
+++ users.py
@@ -0,0 +1,13 @@
+import random
+
+class User():
+    '''
+    class for new user details and login info.
+    '''
+    users_list =[] # empty users list
+
+    def __init__(self,name,email,pin):
+        self.name = name
+        self.email = email
+        self.pin = pin
+    pass
